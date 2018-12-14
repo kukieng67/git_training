68f79e25e2a173f0b9e17f301c620e6813f0ad17
Thang6797 <thangnguyenquoc6797@gmail.com>
2018-12-14 13:11:19 +0700

    demo git

---
+++ hello.rb
@@ -0,0 +1,15 @@
+color = "Red"
+def method1
+	color = 192
+	puts("Color value in method1: ", color)	
+end
+
+def method2
+	color = 255
+	puts("Color value in method2: ", color)
+end
+
+method1
+method2
+
+puts("Color value in outside method: " + color)
